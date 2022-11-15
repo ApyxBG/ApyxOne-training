eb3edd57dcbbc14e6edcc31b959aaeb069b26d3e
nuTarop-dev <github.svasilev@xoxy.net>
2022-11-15 17:26:25 +0200

    fixes navbar buttons

--- src/components/common/NavBarButton.js
+++ src/components/common/NavBarButton.js
@@ -5,7 +5,7 @@ import getIcon from "../../styles/Icons";
 function NavBarButton({ onClick, icon }) {
   return (
     <CircleButton bg="rgb(39,50,78)" onClick={onClick}>
-      {typeof icon === "string" ? getIcon(icon, "#fff", "28px", "25px") : icon}
+      {typeof icon === "string" ? getIcon(icon, "#fff", "70%", "70%") : icon}
     </CircleButton>
   );
 }
